4ed1cd4be11cf0da1a132ec0de5fafc60af5b940
RobTamGon <Alotamgon@gmail.com>
2025-11-25 17:02:02 -0300

    I believe

--- src/app/components/GoogleSignInButton.jsx
+++ src/app/components/GoogleSignInButton.jsx
@@ -3,7 +3,7 @@
 import { signIn, signOut, useSession } from "next-auth/react"
 
 export default function GoogleSignInButton() {
-  const { data: session, status } = useSession()
+  const { data: session, status } = useSession() ?? {};
 
   if (status === "loading") {
     return <p>Verificando sesión...</p>
